876c301769a3fe45ead7d819b5ead1762fa45575
dunt21 <suadmacaulay50@gmail.com>
2025-08-01 11:11:30 +0000

    refactor: update class names and styles in Navbar, Services, and Work components; modify grid template in Tailwind config

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -49,7 +49,10 @@ const Work = ({ isDarkMode }) => {
             whileHover={{ scale: 1.05 }}
             transition={{ duration: 0.3 }}
             key={index}
-            style={{ backgroundImage: `url(${project.bgImage}) ` }}
+            style={{
+              backgroundImage: `url(${project.bgImage}) `,
+              backgroundColor: "orange",
+            }}
             className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group"
           >
             <div className="bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
